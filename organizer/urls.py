87fa76ec9f7b52b360e7ed9fa04a08dae6b8e3bb
lse415 <leesuel0379@gmail.com>
2023-04-20 20:33:20 -0500

    weekly

--- organizer/urls.py
+++ organizer/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from organizer import views
 from django.views.generic import RedirectView
 
+from .views import WeeklyCalendarView
 
 urlpatterns = [
    path('', RedirectView.as_view(url='/login/', permanent=True)),
+   path('weekly-calendar/', WeeklyCalendarView.as_view(), name='weekly-calendar'),
+
    path('calendar/', views.calendar, name="calendar"),
    path('create/', views.create, name="create"),
    path('login/', views.login, name="login"),
--- organizer/views.py
+++ organizer/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from organizer.models import Account
 
+from django.views.generic import TemplateView
+from django.utils import timezone
+from calendar import monthrange
+from datetime import timedelta
+
+class WeeklyCalendarView(TemplateView):
+    template_name = 'weekly_calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get the year and month from the URL parameters or the current date
+        year = int(self.request.GET.get('year', timezone.now().year))
+        month = int(self.request.GET.get('month', timezone.now().month))
+
+        # Calculate the first and last days of the week
+        # (Assuming Sunday is the first day of the week)
+        first_day_of_month = timezone.datetime(year, month, 1)
+        last_day_of_month = timezone.datetime(year, month, monthrange(year, month)[1])
+        first_day_of_week = first_day_of_month - timedelta(days=first_day_of_month.weekday())
+        last_day_of_week = last_day_of_month + timedelta(days=6 - last_day_of_month.weekday())
+
+        # Generate a list of dates representing each day of the week
+        dates = []
+        for i in range(7):
+            date = first_day_of_week + timedelta(days=i)
+            dates.append(date)
+
+        # Add the year, month, first_day_of_week, and last_day_of_week to the context
+        context['year'] = year
+        context['month'] = month
+        context['first_day_of_week'] = first_day_of_week
+        context['last_day_of_week'] = last_day_of_week
+        context['dates'] = dates
+        
+        return context
+
+
+
 def login(request):
     if request.method == 'POST':
         username = str(request.POST.get('username_text_box'))
@@ -26,4 +65,4 @@ def create(request):
     return render(request, 'organizer/create.html', context = None)
 
 def calendar(request, acct_id):
-    return render(request, "organizer/calendar.html", context={'acct_id' : acct_id})
+    return render(request, "organizer/weekly_calendar.html", context={'acct_id' : acct_id})
